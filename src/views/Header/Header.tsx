e2c2d154f84b2ad8c8ec7b7013d6a1b20a825a0a
daniel <danielsilvacr123@gmail.com>
2024-07-12 21:48:33 -0300

    feat: criando os arquivos da views Header

---
+++ src/views/Header/Header.tsx
@@ -0,0 +1,14 @@
+
+
+function Header (){
+    return (
+        <header className='header'>
+            <div className="header__div">
+                <h1>Organização</h1>
+                <h2>Tarefas</h2>
+            </div>
+        </header>
+    )
+}
+
+export default Header 
