ba98eb190964aef5aae8c0e3b5871a7ce8521b83
daniel <danielsilvacr123@gmail.com>
2024-07-15 21:00:37 -0300

    feat: criando o body

---
+++ src/views/Body/Body.tsx
@@ -0,0 +1,13 @@
+import "./index.scss";
+
+function Body (){
+    return (
+        <header className='body'>
+            <div className="body__text">
+                <h1>Otimize seu tempo e se organize com o nosso Planejador Diário.</h1>
+            </div>
+        </header>
+    );
+};
+
+export default Body 
